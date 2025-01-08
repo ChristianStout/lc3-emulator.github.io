729665e314403e5f949e00dd775b22b3bed67798
Christian Stout <christian.allen.stout@gmail.com>
2025-01-08 08:54:33 -0700

    Each instruction now pass a reference to the registers

--- src/vm/instructions.rs
+++ src/vm/instructions.rs
@@ -1,10 +1,11 @@
+use super::registers::Registers;
 
 /*
 Uses the command pattern to execute functions dynamically
 */
 
 pub trait Instruction {
-    fn exe(&self, value: u16);
+    fn exe(&self, value: u16, reg: &mut Registers);
 }
 
 pub struct Add;
@@ -23,85 +24,85 @@ pub struct Str;
 pub struct Trap;
 
 impl Instruction for Add {
-    fn exe(&self, value: u16) {
+    fn exe(&self, value: u16, reg: &mut Registers) {
 
     }
 }
 
 impl Instruction for And {
-    fn exe(&self, value: u16) {
+    fn exe(&self, value: u16, reg: &mut Registers) {
 
     }
 }
 
 impl Instruction for Br {
-    fn exe(&self, value: u16) {
+    fn exe(&self, value: u16, reg: &mut Registers) {
 
     }
 }
 
 impl Instruction for JmpRet {
-    fn exe(&self, value: u16) {
+    fn exe(&self, value: u16, reg: &mut Registers) {
 
     }
 }
 
 impl Instruction for Jsr {
-    fn exe(&self, value: u16) {
+    fn exe(&self, value: u16, reg: &mut Registers) {
 
     }
 }
 
 impl Instruction for Ld {
-    fn exe(&self, value: u16) {
+    fn exe(&self, value: u16, reg: &mut Registers) {
 
     }
 }
 
 impl Instruction for Ldi {
-    fn exe(&self, value: u16) {
+    fn exe(&self, value: u16, reg: &mut Registers) {
 
     }
 }
 
 impl Instruction for Lea {
-    fn exe(&self, value: u16) {
+    fn exe(&self, value: u16, reg: &mut Registers) {
 
     }
 }
 
 impl Instruction for Not {
-    fn exe(&self, value: u16) {
+    fn exe(&self, value: u16, reg: &mut Registers) {
 
     }
 }
 
 impl Instruction for Rti {
-    fn exe(&self, value: u16) {
+    fn exe(&self, value: u16, reg: &mut Registers) {
 
     }
 }
 
 impl Instruction for St {
-    fn exe(&self, value: u16) {
+    fn exe(&self, value: u16, reg: &mut Registers) {
 
     }
 }
 
 impl Instruction for Sti {
-    fn exe(&self, value: u16) {
+    fn exe(&self, value: u16, reg: &mut Registers) {
 
     }
 }
 
 impl Instruction for Str {
-    fn exe(&self, value: u16) {
+    fn exe(&self, value: u16, reg: &mut Registers) {
 
     }
 }
 
 impl Instruction for Trap {
-    fn exe(&self, value: u16) {
+    fn exe(&self, value: u16, reg: &mut Registers) {
 
     }
 }
