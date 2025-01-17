5f924d8acafe623d81b8999a91ae7a42ac48596e
Christian Stout <christian.allen.stout@gmail.com>
2025-01-16 19:07:25 -0700

    Added the regex library for lexing

--- src/asm/lexer.rs
+++ src/asm/lexer.rs
@@ -1,17 +1,17 @@
 use super::asm_ins::OpcodeIns;
+use regex::Regex;
 
 
 pub enum Token {
     Instruction(OpcodeIns),
     Directive,
     Number(i16),
-
 }
 
 
 
 pub struct Lexer {
-
+    
 }
 
 impl Lexer {
