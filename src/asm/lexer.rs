449c87861d994d124dca09bb97ccfc403b9112e8
Christian Stout <christian.allen.stout@gmail.com>
2025-01-16 20:20:46 -0700

    Added a br regex to lexer

--- src/asm/lexer.rs
+++ src/asm/lexer.rs
@@ -3,21 +3,24 @@ use regex::Regex;
 
 
 pub enum Token {
+    Label(String),
     Instruction(OpcodeIns),
-    Directive,
+    Directive(String),
     Number(i16),
 }
 
 
 
 pub struct Lexer {
-    
+    br_regex: Regex,
 }
 
 impl Lexer {
     pub fn new() -> Lexer {
+        let br_regex: Regex = Regex::new(r#"BR[nzp][nzp][nzp]"#).unwrap();
+        
         Lexer {
-
+            br_regex: br_regex,
         }
     }
 
