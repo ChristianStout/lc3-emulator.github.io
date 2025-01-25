4a56a0edcba8798a1c4e92ddba2caa19cd1e2d85
Christian Stout <christian.allen.stout@gmail.com>
2025-01-24 20:39:41 -0700

    Fixed the br regex

--- src/asm/lexer.rs
+++ src/asm/lexer.rs
@@ -17,12 +17,32 @@ pub struct Lexer {
 
 impl Lexer {
     pub fn new() -> Lexer {
-        let br_regex: Regex = Regex::new(r#"BR[nzp][nzp][nzp]"#).unwrap();
+        let br_regex: Regex = Regex::new(r#"^BR[n]?[z]?[p]?"#).unwrap();
         
         Lexer {
             br_regex: br_regex,
         }
     }
+}
 
+#[cfg(test)]
+mod tests {
+    use super::Lexer;
 
+    #[test]
+    fn test_br_regex() {
+        let lexer = Lexer::new();
+
+        assert!(lexer.br_regex.is_match("BR"));
+        assert!(lexer.br_regex.is_match("BRn"));
+        assert!(lexer.br_regex.is_match("BRz"));
+        assert!(lexer.br_regex.is_match("BRnz"));
+        assert!(lexer.br_regex.is_match("BRnp"));
+        assert!(lexer.br_regex.is_match("BRpz"));
+        assert!(lexer.br_regex.is_match("BRnzp"));
+        
+        // assert!(!lexer.br_regex.is_match("BRx"));
+        assert!(!lexer.br_regex.is_match("B R"));
+        // assert!(!lexer.br_regex.is_match("BRR"));
+    }
 }
