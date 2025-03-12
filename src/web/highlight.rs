1719b3ae3d99a13fc58f91452af119ae955a3f72
Christian Stout <christian.allen.stout@gmail.com>
2025-03-12 09:09:59 -0600

    Properly syntax highlighting now

--- src/web/highlight.rs
+++ src/web/highlight.rs
@@ -7,11 +7,12 @@ pub fn highlight_text(text: &str) -> String {
     let mut output: String = String::new();
     let tokens = Lexer::new().run(text.to_string());
     let mut i: usize = 0;
-    let mut curr_line = 0;
+    // let mut curr_line = 0;
 
     for token in tokens {
         
-        fill_gap(text, &mut output, &mut i, Some(&token));
+        output.push_str(&fill_gap(text, &mut i, Some(&token)));
+        i = output.len();
 
         // // add remaining from previous line, matching comments
         // if curr_line < token.line_num {
@@ -23,69 +24,78 @@ pub fn highlight_text(text: &str) -> String {
 
         match token.inner_token {
             TokenType::Instruction(_) => {
-                output.push_str(&text[i..token.from]);
+                // output.push_str(&text[i..token.from]);
                 output.push_str(r#"<span id="highlight-instruction">"#);
                 output.push_str(&token.original_match);
                 output.push_str(r#"</span>"#);
                 i = token.to + 1;
             },
             TokenType::Directive(_) => {
-                output.push_str(&text[i..token.from]);
+                // output.push_str(&text[i..token.from]);
                 output.push_str(r#"<span id="highlight-directive">"#);
                 output.push_str(&token.original_match);
                 output.push_str(r#"</span>"#);
                 i = token.to + 1;
             },
             TokenType::Register(_) => {
-                output.push_str(&text[i..token.from]);
+                // output.push_str(&text[i..token.from]);
                 output.push_str(r#"<span id="highlight-register">"#);
                 output.push_str(&token.original_match);
                 output.push_str(r#"</span>"#);
                 i = token.to + 1;
             },
             TokenType::String(_) => {
-                output.push_str(&text[i..token.from]);
+                // output.push_str(&text[i..token.from]);
                 output.push_str(r#"<span id="highlight-string">"#);
                 output.push_str(&token.original_match);
                 output.push_str(r#"</span>"#);
                 i = token.to + 1;
             },
             _ => {
-                output.push_str(&text[i..=token.to]);
+                output.push_str(&token.original_match);
                 i = token.to + 1;
             }
         } 
     }
 
-    // Also Fill any information ignored by the lexer
-    
-    let mut entered_comment = false;
-
-    for c in text[i..].chars() {
-
-        if c == '\n' {
-            if entered_comment {
-                output.push_str(r#"</span>"#);
-                entered_comment = false;
-            }
-        }
-
-        if c == ';' {
-            entered_comment = true;
-            output.push_str(r#"<span id="highlight-comment">"#);
-        }
-
-        output.push(c);
-    }
-
-    if entered_comment {
-        output.push_str(r#"</span>"#);
-    }
+    output.push_str(&fill_gap(text, &i, None));
 
     return output;
 }
 
-fn fill_gap(text: &str, output: &mut str, i: &usize, maybe_token: Option<&Token>) {
+fn fill_gap(text: &str, i: &usize, maybe_token: Option<&Token>) -> String {
+    let start: usize = *i;
+    let end;
+    match maybe_token {
+        Some(token) => end = token.from,
+        None => end = text.len(),
+    }
+
+    let mut entered_comment = false;
+    let mut stream = "".to_string();
+
+    for (j, c) in text[start..end].chars().enumerate() {
+        if c == ';' {
+            stream.push_str(r#"<span id="highlight-comment">"#);
+            entered_comment = true;
+        }
+
+        if c == '\n' && entered_comment {
+            stream.push_str(r#"</span>"#);
+            entered_comment = false;
+            stream.push(c);
+            continue;
+        }
+
+        stream.push(c);
+
+        if entered_comment && j + start == end - 1 {
+            stream.push_str(r#"</span>"#);
+            entered_comment = false;
+        }
+    }
+
+    return stream;
 
 }
 
@@ -141,7 +151,6 @@ mod tests {
 
         let text = highlight_text(r" hi ; so
             ;a");
-        println!("{}", text);
         assert_eq!(text, r#" hi <span id="highlight-comment">; so</span>
             <span id="highlight-comment">;a</span>"#.to_string());
 
